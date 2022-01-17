bf41f4a20341bcfc64063f542b474fe7d5cd7035
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2022-01-17 14:28:24 +0200

    Define purchasable upgrades as resources.

---
+++ src/data/upgrades/upgrade.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name Upgrade
+
+enum UpgradeType {
+	unknown = 0
+	health = 1,
+	oxygen = 2,
+	ascend_speed = 3,
+	descend_speed = 4
+}
+
+export(int) var id = -1
+export(int) var price = 1
+export(int) var required_progress = 50
+export(int) var strength = 0
+export(int) var type = 0
+export(String) var title = "txt_upgrade_title"
+export(String) var description = "txt_upgrade_description"
+export(Texture) var icon
---
+++ src/data/upgrades/upgrade_registry.gd
@@ -0,0 +1,46 @@
+extends Node
+
+
+const UPGRADES = [
+	preload("res://data/upgrades/health1.tres"),
+	preload("res://data/upgrades/health2.tres"),
+	preload("res://data/upgrades/health3.tres"),
+	preload("res://data/upgrades/oxygen1.tres"),
+	preload("res://data/upgrades/oxygen2.tres"),
+	preload("res://data/upgrades/oxygen3.tres")
+]
+
+var _milestone_map = {}
+
+func _ready() -> void:
+	_load()
+	print(_milestone_map)
+
+
+func get_progress_milestones() -> Array:
+	return _milestone_map.keys().sort()
+
+
+func get_purchased_upgrades() -> Array:
+	var result = []
+	
+	for upgrade in UPGRADES:
+		if UserSaveData.unlocked_upgrades.has(upgrade.id):
+			result.append(upgrade)
+
+	return result
+
+
+func _load() -> void:
+	for upgrade in UPGRADES:
+		if not _milestone_map.has(upgrade.required_progress):
+			_milestone_map[upgrade.required_progress] = []
+		
+		_milestone_map[upgrade.required_progress].append(upgrade)
+	
+	for key in _milestone_map.keys():
+		_milestone_map[key].sort_custom(self, "_upgrade_id_sort")
+
+
+func _upgrade_id_sort(a: Upgrade, b: Upgrade) -> bool:
+	return b.id > a.id
