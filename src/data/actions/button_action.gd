a4719c524e3fda88608ad50f8e279c2149849123
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2022-01-19 13:47:18 +0200

    Define action resources.

---
+++ src/data/actions/button_action.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name ButtonAction
+
+enum Type {
+	unknown = 0,
+	ascend = 1,
+	descend = 2,
+	special = 3
+}
+
+export(String) var title = "txt_action_unknown"
+export(int) var type = 0
