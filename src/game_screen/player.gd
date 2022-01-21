38c8dd769771de3593b9aa47a70687c5c96fa39b
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2022-01-21 23:39:50 +0200

    Character walking animation.

--- src/game_screen/player.gd
+++ src/game_screen/player.gd
@@ -3,14 +3,27 @@ class_name Player
 
 export(float) var speed = 120.0
 
+onready var _animation_player: AnimationPlayer = $"AnimationPlayer"
+onready var _animation_tree: AnimationTree = $"AnimationTree"
+onready var _animation_state: AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/playback")
+
 func _process(delta: float) -> void:
 	var direction = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	direction = direction.normalized()
+
 	if direction == Vector2.ZERO:
-		# idle
+		_animation_state.travel("idle")
 		return
+	
+	_animation_tree.set("parameters/walk/blend_position", direction)
+	_animation_tree.set("parameters/idle/blend_position", direction)
+	_animation_state.travel("walk")
 
 	move_and_slide(direction * speed)
+
+
+func operate() -> void:
+	_animation_player.play()
