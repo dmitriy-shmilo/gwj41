39715acef171f2028dc359723e99aa2299106533
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2022-01-22 02:07:07 +0200

    Animated enemies.

--- src/game_screen/enemy.gd
+++ src/game_screen/enemy.gd
@@ -1,6 +1,17 @@
 extends Collectable
 class_name Enemy
 
+const FRAMES = [
+	preload("res://game_screen/blowfish_frames.tres"),
+	preload("res://game_screen/squid_frames.tres")
+]
+
+onready var _sprite: AnimatedSprite = $"Sprite"
+
+func _ready() -> void:
+	_sprite.frames = FRAMES[randi() % FRAMES.size()]
+	_sprite.frame = randi() % _sprite.frames.get_frame_count("default")
+
 
 func _on_CollisionArea_body_entered(body: Node) -> void:
 	if not body is Submarine:
